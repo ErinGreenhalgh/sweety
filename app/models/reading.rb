87e536fafa510e28b9df4d202b6b67a106925efe
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-11-03 17:48:56 -0600

    add test and method for finding all the readings for a given day * refactor today method so that the #for_day method takes a default parameter of today’s date

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -6,12 +6,20 @@ class Reading < ApplicationRecord
   private
 
   def reading_within_daily_limit
-    if Reading.today.count >= 4
+    if Reading.for_day.count >= 4
       errors.add(:reading, "cannot be entered more than 4 times per day")
     end
   end
 
-  def self.today
-    where(created_at: (Time.zone.now.beginning_of_day..Time.zone.now.end_of_day))
+  def self.for_day(date = Time.zone.now)
+    where(created_at: (date.beginning_of_day..date.end_of_day))
   end
+
+  # def self.month_to_date(date)
+  #   where(created_at: (Time.zone.now.beginning_of_month..Time.zone.now.end_of_day))
+  # end
+  #
+  # def self.monthly(month, year)
+  #   where("extract (month from created_at) = ? AND extract(year from created_at) = ?", month, year)
+  # end
 end
--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -11,4 +11,32 @@ RSpec.describe Reading, type: :model do
 
     expect{ create(:reading) }.to raise_error(/Reading cannot be entered more than 4 times per day/)
   end
+
+  scenario "gives all the readings for today" do
+    readings_today = create_list(:reading, 3)
+    reading_yesterday = create(:reading, created_at: DateTime.now.yesterday)
+    expect(Reading.for_day).to eq readings_today
+    expect(Reading.for_day).not_to include(reading_yesterday)
+  end
+
+  scenario "gives all the readings for a given day" do
+    date = DateTime.now.yesterday
+    readings = [create(:reading, created_at: date),
+                create(:reading, created_at: date),
+                create(:reading, created_at: date)]
+    other = create(:reading)
+    expect(Reading.for_day(date)).to eq readings
+    expect(Reading.for_day(date)).not_to include(other)
+  end
+
+
+  # scenario "gives all the readings for the month to the given date" do
+  # end
+  #
+  # scenario "gives all the readings for a given month and year" do
+  #   reading_last_month = create(:reading, created_at: DateTime.now.last_month)
+  #   readings_this_month = create_list(:reading, 3)
+  #   expect(Reading.monthly())
+  #
+  # end
 end
