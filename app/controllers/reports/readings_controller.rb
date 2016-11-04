a4b3893f99a3572540c2e03eb2bbd56b9196950d
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-11-04 16:04:35 -0600

    add logic in Reports::ReadingsController to determine which methods to call to show report data

--- app/controllers/reports/readings_controller.rb
+++ app/controllers/reports/readings_controller.rb
@@ -6,7 +6,15 @@ class Reports::ReadingsController < ApplicationController
   def results
     @report_type = params[:report_type]
     @date = params[:date]
-    @data = Reading.report_for_day(@date)
+    if @report_type == ["daily"]
+      @data = Reading.report_for_day(@date)
+    end
+    if @report_type == ["month-to-date"]
+      @data = Reading.report_for_month_to_date(@date)
+    end
+    if @report_type == ["monthly"]
+      @data = Reading.report_for_month(@date)
+    end
   end
 
 end
